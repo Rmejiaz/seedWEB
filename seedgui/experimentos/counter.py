b292969e8b2d488ffa8e670464ee7afb0fef8141
Rafael Mejia <rmejiaz@unal.edu.co>
2023-06-21 09:03:34 -0500

    Detection model now working correctly.

--- seedgui/experimentos/counter.py
+++ seedgui/experimentos/counter.py
@@ -4,9 +4,9 @@ import numpy as np
 
 class Counter:
     def __init__(self, model_path):
-        
+
         if model_path[-2:] == "h5":
-        
+
             self.model = tf.keras.models.load_model(model_path)
             self.type = "tf"
         elif model_path[-6:]=="tflite":
@@ -15,114 +15,101 @@ class Counter:
             self.input_details = self.interpreter.get_input_details()
             self.output_details = self.interpreter.get_output_details()
             self.type = "tflite"
-        
-    def letterbox(self, im, new_shape=(640, 640), color=(114, 114, 114), auto=True, scaleup=True, stride=32):
-        # Resize and pad image while meeting stride-multiple constraints
-        shape = im.shape[:2]  # current shape [height, width]
-        if isinstance(new_shape, int):
-            new_shape = (new_shape, new_shape)
+    def non_max_suppression_fast(self, boxes, classes, overlapThresh = 0.8):
+        # Return an empty list, if no boxes given
+        if len(boxes) == 0:
+            return [], []
 
-        # Scale ratio (new / old)
-        r = min(new_shape[0] / shape[0], new_shape[1] / shape[1])
-        if not scaleup:  # only scale down, do not scale up (for better val mAP)
-            r = min(r, 1.0)
 
-        # Compute padding
-        new_unpad = int(round(shape[1] * r)), int(round(shape[0] * r))
-        dw, dh = new_shape[1] - new_unpad[0], new_shape[0] - new_unpad[1]  # wh padding
+        new_boxes = boxes.copy()
 
-        if auto:  # minimum rectangle
-            dw, dh = np.mod(dw, stride), np.mod(dh, stride)  # wh padding
+        new_boxes[:, 0] = boxes[:, 0] - boxes[:, 2]/2
+        new_boxes[:, 1] = boxes[:, 1] - boxes[:, 3]/2
+        new_boxes[:, 2] = boxes[:, 0] + boxes[:, 2]/2
+        new_boxes[:, 3] = boxes[:, 1] + boxes[:, 3]/2
 
-        dw /= 2  # divide padding into 2 sides
-        dh /= 2
 
-        if shape[::-1] != new_unpad:  # resize
-            im = cv2.resize(im, new_unpad, interpolation=cv2.INTER_LINEAR)
-        top, bottom = int(round(dh - 0.1)), int(round(dh + 0.1))
-        left, right = int(round(dw - 0.1)), int(round(dw + 0.1))
-        im = cv2.copyMakeBorder(im, top, bottom, left, right, cv2.BORDER_CONSTANT, value=color)  # add border
-        return im, r, (dw, dh)
 
-    
-    def predict(self, img):
+        x1 = new_boxes[:, 0]  # x coordinate of the top-left corner
+        y1 = new_boxes[:, 1]  # y coordinate of the top-left corner
+        x2 = new_boxes[:, 2]  # x coordinate of the bottom-right corner
+        y2 = new_boxes[:, 3]  # y coordinate of the bottom-right corner
+
+        # Compute the area of the bounding new_boxes and sort the bounding
+        # new_Boxes by the bottom-right y-coordinate of the bounding box
+        areas = (x2 - x1 + 1) * (y2 - y1 + 1) # We add 1, because the pixel at the start as well as at the end counts
+        # The indices of all new_boxes at start. We will redundant indices one by one.
+        indices = np.arange(len(x1))
+        for i,box in enumerate(new_boxes):
+            # Create temporary indices  
+            temp_indices = indices[indices!=i]
+            # Find out the coordinates of the intersection box
+            xx1 = np.maximum(box[0], new_boxes[temp_indices,0])
+            yy1 = np.maximum(box[1], new_boxes[temp_indices,1])
+            xx2 = np.minimum(box[2], new_boxes[temp_indices,2])
+            yy2 = np.minimum(box[3], new_boxes[temp_indices,3])
+            # Find out the width and the height of the intersection box
+            w = np.maximum(0, xx2 - xx1  + 1)
+            h = np.maximum(0, yy2 - yy1 + 1)
+            # compute the ratio of overlap
+            overlap = (w * h) / (areas[temp_indices] + areas[i] - (w*h))
+            # if the actual boungding box has an overlap bigger than treshold with any other box, remove it's index 
+             
+            if np.any(overlap > overlapThresh):
+                indices = indices[indices != i]
+                
+        #return only the boxes at the remaining indices
+        return boxes[indices].astype(int), classes[indices]
+
+    def predict(self, image):
+
+        img = image.copy()
 
         if self.type == "tflite":
-            # Preprocess the input image
-            input_shape = self.input_details[0]['shape'][1:3]
-            # resized_image = cv2.resize(img, input_shape)
 
-            image, ratio, dwdh = self.letterbox(img, auto=False)
-            print("ratio: ", ratio)
-            print("dwdh: ", dwdh)
-            # image = image.transpose((2, 0, 1))
-            image = np.expand_dims(image, 0)
-            image = np.ascontiguousarray(image)
+            # Load and preprocess the image
 
-            input_data = image.astype(np.float32)
-            input_data /= 255
+            image_resized = cv2.resize(img, (self.input_details[0]['shape'][2], self.input_details[0]['shape'][1]))
+            image_normalized = image_resized / 255.0
+            image_input = np.expand_dims(image_normalized, axis=0).astype(self.input_details[0]['dtype'])
 
-
-
-            # input_data = np.expand_dims(resized_image, axis=0)
-            # input_data = input_data.astype('float32') / 255.
-            print("Input data shape: ",input_data.shape)
-            print("Input data max: ",input_data.max())
-            print("Input data min:",input_data.min())
-            
-            # Run the detection
-            self.interpreter.set_tensor(self.input_details[0]['index'], input_data)
+            # Run the inference
+            self.interpreter.set_tensor(self.input_details[0]['index'], image_input)
             self.interpreter.invoke()
-            self.output_data = self.interpreter.get_tensor(self.output_details[0]['index'])
+            output_data = self.interpreter.get_tensor(self.output_details[0]['index'])
 
-            # # Process the output
-            class_indexes = self.output_data[:, 5,:].argmax()
-            class_ids = self.output_data[:]
-            print("Class ids: ", class_ids)
-            confidences = self.output_data[..., 4]
-            print(self.output_data)
-            print(self.output_data[...,0:4])
-            boxes = (self.output_data[..., 0:4] * img.shape[0])/100
-            print("boxes:", boxes)
+            output_data = output_data[0].T
 
-            # Filter out weak detections
-            mask = confidences > 50
-            class_ids = class_ids[mask]
-            print("Class ids: ", class_ids)
-            confidences = confidences[mask]
-            boxes = boxes[mask]
-            print(boxes)
-            print(confidences)
+            # Extract bounding box coordinates
+            boxes = output_data[:, 0:4]
+            scores = output_data[:, 4:]
+            valid_indices = np.where(scores > 0.5)[0]
+
+            valid_boxes = boxes[valid_indices]
+            classes = scores.argmax(axis=1)[valid_indices]
 
 
-            # Get the bounding boxes and class labels.
-            boxes = self.output_data[:, :, 0:4]
-            classes = self.output_data[:, :, 5:]
-            object_count = {'germinated':0, 'no_germinated':0}
-            # Display the predictions.
-            for i in range(len(boxes)):
-                for box, cls in zip(boxes[i], classes[i]):
-                    if cls[np.argmax(cls)] > 0.5:
-                        x1, y1, x2, y2 = box
-                        cv2.rectangle(img, (int(x1), int(y1)), (int(x2), int(y2)), (0, 255, 0), 2)
-                        cv2.putText(img, str(np.argmax(cls)), (int(x1), int(y1)), cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 0, 255), 2)
-                        
-                        # if cls_id == 0:
-                        #     object_count['Germinated'] += 1
-                        # elif cls_id == 1:
-                        #     object_count['No_germinated'] += 1
+            valid_boxes_2, valid_classes_2 = self.non_max_suppression_fast(valid_boxes, classes)
+            
+    
+            # Draw bounding boxes on the image
+            for box, class_ in zip(valid_boxes_2, valid_classes_2):
+                x, y, w, h = box
+                x = int(x * img.shape[1]/self.input_details[0]['shape'][2])
+                y = int(y * img.shape[0]/self.input_details[0]['shape'][1])
+                w = int(w * img.shape[1]/self.input_details[0]['shape'][2])
+                h = int(h * img.shape[0]/self.input_details[0]['shape'][1])
+                if class_ == 0:
+                  cv2.rectangle(img, (int(x - w/2), int(y - h/2)), (int(x + w/2), int(y + h/2)), (0, 255, 0), 2)
+                else:
+                  cv2.rectangle(img, (int(x - w/2), int(y - h/2)), (int(x + w/2), int(y + h/2)), (255, 0, 0), 2)
 
+
+            object_count = {"germinated": np.count_nonzero(valid_classes_2==0), "no_germinated": np.count_nonzero(valid_classes_2==1)}
 
             # Return the result image and object count
 
-            ori_images = [img.copy()]
-            names = ["No germinada", "Germinada"]
-            colors = {name:[np.random.randint(0, 255) for _ in range(3)] for i,name in enumerate(names)}
-
-
-            result = img.copy()
-            
-            return result, object_count
+            return img, object_count
 
 
 
